0f98347b65787b7870c071d01689c329cfdb81ef
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2020-03-23 11:53:54 +0300

    add thunk for create book in bookReducer

--- src/Redux/bookReducer.js
+++ src/Redux/bookReducer.js
@@ -3,6 +3,7 @@ import {API} from "../API/api";
 export const SET_BOOKS = 'App/bookReducer/SET_BOOKS';
 export const DELETE_BOOK = 'App/bookReducer/DELETE_BOOKS'
 export const UPDATE_BOOK = 'App/bookReducer/UPDATE_BOOK'
+export const ADD_BOOK = 'App/bookReducer/ADD_BOOK'
 
 const initialState = {
     books: []
@@ -12,6 +13,8 @@ export const bookReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_BOOKS:
             return {...state, books: action.books.map(b => ({...b}))}
+        case ADD_BOOK:
+            return {...state, books: [...state.books, action.book]}
         case DELETE_BOOK:
             return {...state, books: state.books.filter(b => b.id !== action.bookId)}
         case UPDATE_BOOK:
@@ -31,6 +34,7 @@ export const bookReducer = (state = initialState, action) => {
 }
 
 export const setBooksSuccess = (books) => ({type: SET_BOOKS, books})
+export const createBookSuccess = (book) => ({ type: ADD_BOOK, book })
 export const deleteBookSuccess = (bookId) => ({type: DELETE_BOOK, bookId})
 export const updateBookSuccess = (bookId, book) => ({type: UPDATE_BOOK, bookId, book})
 
@@ -49,4 +53,9 @@ export const updateBook = (book, bookId) => async (dispatch) => {
     await API.updateBook(book, bookId)
     dispatch(updateBookSuccess(book, bookId))
     dispatch(setBooks())
+}
+
+export const createBook = (bookBody) => async (dispatch) => {
+    let book = await API.addBook(bookBody)
+    dispatch(createBookSuccess(book))
 }
